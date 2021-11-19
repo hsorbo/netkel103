fac23d60aa6f289d1ca434ba0d9da1436987a98f
Håvard Sørbø <havard@hsorbo.no>
2021-11-19 20:05:19 +0100

    CLI improvements (#2)

--- KelCli/Program.fs
+++ KelCli/Program.fs
@@ -4,57 +4,164 @@ open NetKel103.Unfinished
 open NetKel103.Wire
 
 open System.Net
+open Microsoft.FSharp.Reflection
 
-type Measure =
-    | Voltage = 1
-    | Current = 2
-    | Power = 3
-    | Running = 4
-    | BatteryCapacity = 5
+let sndMap f (x, y) = (x, f y)
+let fstMap f (x, y) = (f x, y)
+
+let queryCommands =
+    knownCommands
+    |> List.filter (fun x -> CommandType.canQuery x.Type)
+
+let setCommands =
+    knownCommands
+    |> List.filter (fun x -> CommandType.canSet x.Type)
 
 type Arguments =
-    | Ip of host:string * port:int
-    | Serial of serial:string * baud:int
-    | Sample of Measure list
+    | Ip of host: string * port: int
+    | Serial of serial: string * baud: int
+    | Get of string list
+    | Set of key: string * value: string
+    | Json
+    | Repl
+    | Net_Detect
     interface IArgParserTemplate with
         member s.Usage =
             match s with
-            | Sample _ -> "Sample measurement(s)"
+            | Repl -> "REPL"
             | Serial _ -> "Serial connection"
             | Ip _ -> "Network connection"
+            | Net_Detect -> "Search network"
+            | Json -> "json"
+            | Set _ ->
+                setCommands
+                |> List.map (fun x -> sprintf "%A" x.Command)
+                |> join "\n"
+            | Get _ ->
+                queryCommands
+                |> List.map (fun x -> sprintf "%A" x.Command)
+                |> join "\n"
+
+let fromString<'a> (s: string) =
+    match FSharpType.GetUnionCases typeof<'a>
+          |> Array.filter (fun case -> case.Name = s)
+        with
+    | [| case |] -> Some(FSharpValue.MakeUnion(case, [||]) :?> 'a)
+    | _ -> None
+
+
+let repl querier =
+    ReadLine.AutoCompletionHandler <-
+        { new IAutoCompleteHandler with
+            member _.Separators = [| 'A' |]
+
+            member _.Separators
+                with set (value) = ()
+
+            member _.GetSuggestions(text: string, index: int) : string array =
+                knownCommands
+                |> List.filter (fun x -> x.Raw.StartsWith(text))
+                |> List.map (fun x -> x.Raw)
+                |> List.toArray }
+
+    let prompt _ = ReadLine.Read("SCPI>")
+    printfn "Type exit to exit"
+
+    Seq.initInfinite prompt
+    |> Seq.takeWhile (fun x -> x <> "exit")
+    |> Seq.iter (fun cmd ->
+        ReadLine.AddHistory cmd
+        querier (sprintf "%s\n" cmd) |> printfn "%s")
+
+let query getList querier json =
+    let getList' =
+        if getList |> List.contains "all" then
+            queryCommands |> List.map (fun x -> x.Command)
+        else
+            getList
+            |> List.map fromString<Commands>
+            |> List.choose id
+            |> List.distinct
+
+    let queryResponse = getList' |> Seq.map (fun x -> (x, querier x))
+
+    let colorize =
+        function
+        | FloatWithUnitValue (x, d) -> ConsoleColor.Blue
+        | OnOffValue x ->
+            match x with
+            | On -> ConsoleColor.Green
+            | Off -> ConsoleColor.Red
+        | StringValue x -> ConsoleColor.Yellow
+        | Nothing -> ConsoleColor.Gray
+        | NumericValue x -> ConsoleColor.Magenta
+        | ModeValue m -> ConsoleColor.Cyan
+
+    if json |> not then
+        queryResponse
+        |> Seq.iter (fun (cmd, x) ->
+
+            let prev = Console.ForegroundColor
+            printf "%A: " cmd
+            Console.ForegroundColor <- colorize x
+            printfn "%s" (CommandValue.toString x)
+            Console.ForegroundColor <- prev)
+    else
+        queryResponse
+        |> Seq.map (fstMap (sprintf "%A"))
+        |> Seq.map (sndMap (CommandValue.toPrimitives))
+        |> Map.ofSeq
+        |> System.Text.Json.JsonSerializer.Serialize
+        |> printfn "%s"
+
+let setter setter key value =
+    match fromString<Commands> (key) with
+    | None -> failwith "unknown command"
+    | Some cmd ->
+        let info = cmd |> toInfo
+
+        match CommandType.argType info.Type with
+        | None -> failwith "no set"
+        | Some x -> setter cmd (CommandValue.fromString value x)
+
+    ()
 
 [<EntryPoint>]
 let main argv =
-    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
-    let parser = ArgumentParser.Create<Arguments>(programName = "kelcli", errorHandler = errorHandler)
-    let cmd = parser.ParseCommandLine ()  
-    
-    let c = 
-        cmd.TryGetResult Ip 
-        |> Option.map (fun (ip, port) -> IPEndPoint(IPAddress.Parse(ip), port))
-        |> Option.get
-    use client = new ExperimentalUdpClient(c)
-    
-    let sample = cmd.TryGetResult(Sample) |> Option.defaultValue List.empty
-    if(sample |> List.isEmpty |> not) then 
-        let measurements = [
-            (Measure.Voltage, MeasureVoltage, "voltage", "V")
-            (Measure.Current, MeasureAmp, "current", "A")
-            (Measure.Power, MeasurePower, "watts", "W")
-            (Measure.Running, Input, "running", "")
-            (Measure.BatteryCapacity, BatteryCapacity, "batt-ah", "A")
-        ]
-        let a = 
-            measurements
-                |> List.filter (fun (x,_,_,_) -> List.contains x sample)
-                |> Seq.map (fun (_,cmd,c,d) -> (client.Query(cmd),c,d))
-                //|> Seq.takeWhile (fun (x,_,_) -> )
-                |> Seq.map (fun (response,c,d) -> 
-                    match response with
-                    | FloatWithUnitValue(x, _) -> sprintf "%s: %g%s" c x d
-                    | OnOffValue x -> sprintf "%s: %s" c (if x = On then "on" else "off")
-                    | _ -> sprintf "%s: Error" c
-                )
-                |> (fun x -> (String.Join(", ", x)))
-        printfn "%s %s" (DateTime.Now.ToLongTimeString()) a
+    let errorHandler =
+        ProcessExiter(
+            colorizer =
+                function
+                | ErrorCode.HelpText -> None
+                | _ -> Some ConsoleColor.Red
+        )
+
+    let parser =
+        ArgumentParser.Create<Arguments>(programName = "kelcli", errorHandler = errorHandler)
+
+    let cmd = parser.ParseCommandLine()
+
+    if (cmd.Contains(Net_Detect)) then
+        printfn "Searching for KEL10x"
+
+        NetworkUtils.searchNetkel ()
+        |> Seq.iter (printfn "Found: %s")
+    else
+        let ip, port = cmd.GetResult Ip
+
+        use client = new ExperimentalUdpClient(IPEndPoint(IPAddress.Parse(ip), port))
+
+        if (cmd.Contains(Repl)) then
+            repl client.Raw
+        elif (cmd.Contains(Get)) then
+            match cmd.TryGetResult(Get) with
+            | Some x -> query x client.Query (cmd.Contains(Json))
+            | None _ -> printfn "unable to get"
+        elif (cmd.Contains(Set)) then
+            match cmd.TryGetResult(Set) with
+            | Some (k, v) -> setter (client.Set) k v
+            | None _ -> printfn "unable to set"
+
+        ()
+
     0
--- NetKel103/Kel103.fs
+++ NetKel103/Kel103.fs
@@ -1,25 +1,45 @@
 ﻿namespace NetKel103
+
 open System
 
 module Wire =
     open System.Text.RegularExpressions
+
     type ValueType =
         | Text
         | OnOff
         | NumericWithUnit
         | Numeric
         | Mode
+        | NoArg
 
     type CommandType =
-        | Get of ValueType
-        | Set
-        | Both of ValueType
+        | Get of returns: ValueType
+        | Set of arg: ValueType
+        | Both of arg: ValueType * returns: ValueType
 
-    type Commands = 
+    module CommandType =
+        let argType =
+            function
+            | Set x
+            | Both (x, _) -> x |> Some
+            | Get _ -> None
+
+        let canQuery =
+            function
+            | Set _ -> false
+            | _ -> true
+
+        let canSet =
+            function
+            | Get _ -> false
+            | _ -> true
+
+    type Commands =
         | ProductInformation
         | StoreToUnit
         | RecallStorageUnit
-        | SimulateExternalTrigger 
+        | SimulateExternalTrigger
         | SystemBuzzer
         | SystemBaud
         | DeviceStatus
@@ -40,7 +60,7 @@ module Wire =
         | MeasureVoltage
         | MeasureAmp
         | MeasurePower
-        | OutputAllSteps 
+        | OutputAllSteps
         | RecallList
         | OutputAllStepsOcp
         | RecallOcp
@@ -61,116 +81,204 @@ module Wire =
         | KeypadLock
         | UndocumentedComp
         | UndocumentedExit
- 
-    let private doDef = function
-        | ProductInformation        -> ("*IDN",          Get(Text))
-        | StoreToUnit               -> ("*SAV",          Set) //<NR1> 1-100
-        | RecallStorageUnit         -> ("*RCL",          Set) //<NR1> 1-100
-        | SimulateExternalTrigger   -> ("*TRG",          Set) 
-        | SystemBuzzer              -> (":SYST:BEEP",    Both(OnOff))
-        | SystemBaud                -> (":SYST:BAUD",    Both(Numeric))
-        | DeviceStatus              -> (":STAT",         Get(Text)) //The first byte is the buzzer status and the second byte is the baud rate; other bytes are to be determined.
-        | Input                     -> (":INP",          Both(OnOff))
-        | Voltage                   -> (":VOLT",         Both(NumericWithUnit)) //Changes Func Cv
-        | VoltageMax                -> (":VOLT:UPP",     Both(NumericWithUnit))
-        | VoltageMin                -> (":VOLT:LOW",     Both(NumericWithUnit))
-        | Current                   -> (":CURR",         Both(NumericWithUnit)) //Changes Func Cc
-        | CurrentMax                -> (":CURR:UPP",     Both(NumericWithUnit))
-        | CurrentMin                -> (":CURR:LOW",     Both(NumericWithUnit))
-        | Resistance                -> (":RES",          Both(NumericWithUnit)) //Changes Func Cr
-        | ResistanceMax             -> (":RES:UPP",      Both(NumericWithUnit))
-        | ResistanceMin             -> (":RES:LOW",      Both(NumericWithUnit))
-        | Power                     -> (":POW",          Both(NumericWithUnit)) //Changes Func Cw
-        | PowerMax                  -> (":POW:UPP",      Both(NumericWithUnit))
-        | PowerMin                  -> (":POW:LOW",      Both(NumericWithUnit))
-        | Function                  -> (":FUNC",         Both(Mode)) //Only can switch CV, CC, CR, CW. Can query CV, CC, CR, CW, that in continuous mode, pulse, flip, battery and all the other modes.
-        | MeasureVoltage            -> (":MEAS:VOLT",    Get(NumericWithUnit))
-        | MeasureAmp                -> (":MEAS:CURR",    Get(NumericWithUnit))
-        | MeasurePower              -> (":MEAS:POW",     Get(NumericWithUnit))
-        | OutputAllSteps            -> (":LIST",         Set) 
-        | RecallList                -> (":RCL:LIST",     Both(Text)) //Recall the query unit before query, or an unknown condition occurs.
-        | OutputAllStepsOcp         -> (":OCP",          Set)
-        | RecallOcp                 -> (":RCL:OCP",      Both(Text))
-        | OutputAllStepsOpp         -> (":OPP",          Set)
-        | RecallOpp                 -> (":RCL:OPP",      Both(Text))
-        | OutputAllStepsBattery     -> (":BATT",         Set)
-        | RecallBattery             -> (":RCL:BATT",     Both(Text))
-        | BatteryTime               -> (":BATT:TIM",     Get(NumericWithUnit))
-        | BatteryCapacity           -> (":BATT:CAP",     Get(NumericWithUnit))
-        | DynamicTestMode           -> (":DYN",          Both(Text))
-        | SystemIpAddress           -> (":SYST:IPAD",    Both(Text))
-        | SystemSubnetMask          -> (":SYST:SMASK",   Both(Text))
-        | SystemGateway             -> (":SYST:GATE",    Both(Text))
-        | SystemDhcp                -> (":SYST:DHCP",    Both(Numeric))
-        | SystemMacAddress          -> (":SYST:MAC",     Get(Text))
-        | SystemPort                -> (":SYST:PORT",    Both(Numeric))
-        | SystemDeviceInfo          -> (":SYST:DEVINFO", Get(Text))
-        | KeypadLock                -> (":SYST:LOCK",    Both(OnOff)) 
-        | UndocumentedComp          -> (":COMP",         Both(Text)) 
-        | UndocumentedExit          -> (":EXIT",         Both(Text)) //ON|OFF
 
-    type OnOff = | On | Off
-        with 
-            static member Mapping = [("ON", On);("OFF", Off)] |> Map.ofList
-            static member Parse s = Map.find s OnOff.Mapping
-    
-    type Measure = | A | Ah | V | W | Ohm
-        with 
-            static member Mapping = [("A", A);("AH", Ah);("V", V);("W", W);("OHM", Ohm)] |> Map.ofList
-            static member Parse s = Map.find s Measure.Mapping
-            static member ParseSpecial s = 
-                let mtch = Regex("^(?<number>(\d|\.)+)(?<unit>A|V|W|OHM|AH)$").Match(s)
-                (mtch.Groups.["number"].Value |> float, mtch.Groups.["unit"].Value |> Measure.Parse)
+    type CommandInfo = { Command: Commands; Raw: string; Type: CommandType }
 
-    type Mode = | Cc | Cv | Cr | Cw
-        with 
-            static member Mapping = [("CC", Cc);("CV", Cv);("CR", Cr);("CW", Cw)] |> Map.ofList
-            static member Parse s = Map.find s Mode.Mapping
+    let knownCommands =
+        [ (ProductInformation, "*IDN", Get(Text))
+          (StoreToUnit, "*SAV", Set(Numeric)) //<NR1> 1-100
+          (RecallStorageUnit, "*RCL", Set(Numeric)) //<NR1> 1-100
+          (SimulateExternalTrigger, "*TRG", Set(NoArg))
+          (SystemBuzzer, ":SYST:BEEP", Both(OnOff, OnOff))
+          (SystemBaud, ":SYST:BAUD", Both(Numeric, Numeric))
+          (DeviceStatus, ":STAT", Get(Text)) //The first byte is the buzzer status and the second byte is the baud rate; other bytes are to be determined.
+          (Input, ":INP", Both(OnOff, OnOff))
+          (Voltage, ":VOLT", Both(NumericWithUnit, NumericWithUnit)) //Changes Func Cv
+          (VoltageMax, ":VOLT:UPP", Both(NumericWithUnit, NumericWithUnit))
+          (VoltageMin, ":VOLT:LOW", Both(NumericWithUnit, NumericWithUnit))
+          (Current, ":CURR", Both(NumericWithUnit, NumericWithUnit)) //Changes Func Cc
+          (CurrentMax, ":CURR:UPP", Both(NumericWithUnit, NumericWithUnit))
+          (CurrentMin, ":CURR:LOW", Both(NumericWithUnit, NumericWithUnit))
+          (Resistance, ":RES", Both(NumericWithUnit, NumericWithUnit)) //Changes Func Cr
+          (ResistanceMax, ":RES:UPP", Both(NumericWithUnit, NumericWithUnit))
+          (ResistanceMin, ":RES:LOW", Both(NumericWithUnit, NumericWithUnit))
+          (Power, ":POW", Both(NumericWithUnit, NumericWithUnit)) //Changes Func Cw
+          (PowerMax, ":POW:UPP", Both(NumericWithUnit, NumericWithUnit))
+          (PowerMin, ":POW:LOW", Both(NumericWithUnit, NumericWithUnit))
+          (Function, ":FUNC", Both(Mode, Mode)) //Only can switch CV, CC, CR, CW. Can query CV, CC, CR, CW, that in continuous mode, pulse, flip, battery and all the other modes.
+          (MeasureVoltage, ":MEAS:VOLT", Get(NumericWithUnit))
+          (MeasureAmp, ":MEAS:CURR", Get(NumericWithUnit))
+          (MeasurePower, ":MEAS:POW", Get(NumericWithUnit))
+          (OutputAllSteps, ":LIST", Set(Text))
+          (RecallList, ":RCL:LIST", Both(Numeric, Text)) //Recall the query unit before query, or an unknown condition occurs.
+          (OutputAllStepsOcp, ":OCP", Set(Text))
+          (RecallOcp, ":RCL:OCP", Both(Numeric, Text))
+          (OutputAllStepsOpp, ":OPP", Set(Text))
+          (RecallOpp, ":RCL:OPP", Both(Numeric, Text))
+          (OutputAllStepsBattery, ":BATT", Set(Text))
+          (RecallBattery, ":RCL:BATT", Both(Numeric, Text))
+          (BatteryTime, ":BATT:TIM", Get(NumericWithUnit))
+          (BatteryCapacity, ":BATT:CAP", Get(NumericWithUnit))
+          (DynamicTestMode, ":DYN", Both(Text, Text))
+          (SystemIpAddress, ":SYST:IPAD", Both(Text, Text))
+          (SystemSubnetMask, ":SYST:SMASK", Both(Text, Text))
+          (SystemGateway, ":SYST:GATE", Both(Text, Text))
+          (SystemDhcp, ":SYST:DHCP", Both(Numeric, Numeric))
+          (SystemMacAddress, ":SYST:MAC", Get(Text))
+          (SystemPort, ":SYST:PORT", Both(Numeric, Numeric))
+          (SystemDeviceInfo, ":SYST:DEVINFO", Get(Text))
+          (KeypadLock, ":SYST:LOCK", Both(OnOff, OnOff))
+          (UndocumentedComp, ":COMP", Both(Text, Text))
+          (UndocumentedExit, ":EXIT", Both(Text, Text)) ] //ON|OFF
+        |> List.map (fun (cmd, raw, cmdtype) -> { Command = cmd; Raw = raw; Type = cmdtype })
+
+    let toInfo (cmd: Commands) =
+        knownCommands
+        |> List.filter (fun x -> cmd = x.Command)
+        |> List.exactlyOne
+
+    type OnOff =
+        | On
+        | Off
+
+    module OnOff =
+        let fromString =
+            function
+            | "ON" -> On
+            | "OFF" -> Off
+            | _ -> failwith "Unknown value"
+
+        let toString =
+            function
+            | On -> "ON"
+            | Off -> "OFF"
+
+    type Measure =
+        | A
+        | Ah
+        | V
+        | W
+        | Ohm
+
+    module Measure =
+        let fromString =
+            function
+            | "A" -> A
+            | "AH" -> Ah
+            | "V" -> V
+            | "W" -> W
+            | "OHM" -> Ohm
+            | _ -> failwith "unknown string"
+
+        let toString =
+            function
+            | A -> "A"
+            | Ah -> "AH"
+            | V -> "V"
+            | W -> "W"
+            | Ohm -> "OHM"
+
+        let parseSpecial s =
+            let mtch =
+                Regex("^(?<number>(\d|\.)+)(?<unit>A|V|W|OHM|AH)$")
+                    .Match(s)
+
+            (mtch.Groups.["number"].Value |> float, mtch.Groups.["unit"].Value |> fromString)
+
+    type Mode =
+        | Cc
+        | Cv
+        | Cr
+        | Cw
+
+    module Mode =
+        let fromString =
+            function
+            | "CC" -> Cc
+            | "CV" -> Cv
+            | "CR" -> Cr
+            | "CW" -> Cw
+            | _ -> failwith "parse error"
+
+        let toString =
+            function
+            | Cc -> "CC"
+            | Cv -> "CV"
+            | Cr -> "CR"
+            | Cw -> "CW"
 
     type CommandValue =
         | Nothing
         | StringValue of string
-        | FloatWithUnitValue of number:float * unit:Measure
+        | FloatWithUnitValue of number: float * unit: Measure
         | NumericValue of int
         | OnOffValue of OnOff
         | ModeValue of Mode
 
-    let private createResponse cmdType (value:string) =
-        let sanitized = value.Trim()
-        let responseType = match cmdType with | Get t -> t | Both t -> t | _ -> Text
-        if String.IsNullOrWhiteSpace(sanitized) then Nothing 
-        else
-            match responseType with
-            | Text -> StringValue(sanitized)
-            | NumericWithUnit -> sanitized |> Measure.ParseSpecial |> FloatWithUnitValue
-            | OnOff -> OnOff.Parse(sanitized) |> OnOffValue
-            | Mode -> Mode.Parse(sanitized) |> ModeValue
-            | Numeric -> sanitized |> int |> NumericValue
-    
-    type CommandDefinition = { 
-        Prefix:string
-        Query:string
-        Type:CommandType
-        ResponseHandler:string->CommandValue
-        HappyWithResponse:string->bool
-        GenerateRaw:string->string
-    }
+    module CommandValue =
+        let fromString (value: string) valueType =
+            let sanitized = value.Trim()
 
-    let toDef cmd =
-        let prefix, cmdType = doDef cmd
-        {
-            Prefix = prefix
-            Query = sprintf "%s?\n" prefix
-            Type = cmdType
-            ResponseHandler = (createResponse cmdType)
-            HappyWithResponse = (fun x ->
-                match cmd with
+            if String.IsNullOrWhiteSpace(sanitized) then
+                Nothing
+            else
+                match valueType with
+                | Text -> StringValue(sanitized)
+                | NumericWithUnit ->
+                    sanitized
+                    |> Measure.parseSpecial
+                    |> FloatWithUnitValue
+                | OnOff -> OnOff.fromString sanitized |> OnOffValue
+                | Mode -> Mode.fromString sanitized |> ModeValue
+                | Numeric -> sanitized |> int |> NumericValue
+                | NoArg -> Nothing
+
+        let toString =
+            function
+            | StringValue (x) -> x
+            | FloatWithUnitValue (n, v) -> sprintf "%f%s" n (v |> Measure.toString)
+            | OnOffValue x -> x |> OnOff.toString
+            | ModeValue x -> x |> Mode.toString
+            | Nothing -> ""
+            | NumericValue x -> sprintf "%i" x
+
+        let toPrimitives =
+            function
+            | FloatWithUnitValue (x, d) -> box x
+            | OnOffValue x ->
+                match x with
+                | On -> true
+                | Off -> false
+            | StringValue x -> x
+            | Nothing -> null
+            | ModeValue m -> Mode.toString m
+            | NumericValue x -> x
+
+    let private createResponse cmdType (value: string) =
+        let responseType =
+            match cmdType with
+            | Get t -> t
+            | Both (arg, ret) -> ret
+            | _ -> Text
+
+        CommandValue.fromString value responseType
+
+    type CommandDefinition =
+        { Prefix: string
+          Query: string
+          Type: CommandType
+          ResponseHandler: string -> CommandValue
+          HappyWithResponse: string -> bool
+          GenerateRaw: string -> string }
+
+    let toDef (info: CommandInfo) =
+        { Prefix = info.Raw
+          Query = sprintf "%s?\n" info.Raw
+          Type = info.Type
+          ResponseHandler = (createResponse info.Type)
+          HappyWithResponse =
+            (fun x ->
+                match info.Command with
                 | SystemDeviceInfo -> x |> Seq.filter (fun x -> x = '\n') |> Seq.length > 6
                 | _ -> x.Contains("\n"))
-            GenerateRaw = (fun x -> sprintf "%s %s\n" prefix x )
-        }
-
-    let canQuery cmd =
-        match (cmd |> doDef |> snd) with
-        | Set _ -> false
-        | _ -> true
+          GenerateRaw = (fun x -> sprintf "%s %s\n" info.Raw x) }
